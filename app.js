68d7bd7dc8b0661aaf85d6dfe227e86c024c3a7a
Aditya Panda <adityapandaofficial@gmail.com>
2021-05-12 16:11:28 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("this is working");
+// var userName = prompt("Hey Tell me you name?")
+
+// var welcomeMessage = "Welcome "+userName;
+
+// alert(welcomeMessage)
+
+var buttonTranslate = document.querySelector("#btn-translate")
+var textInput = document.querySelector("#txt-input")
+
+// console.log(buttonTranslate); 
+
+buttonTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked")
+    console.log("input", textInput.value);
+})
