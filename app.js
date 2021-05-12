bbb0ceca593d78e7277bb762be78b1e60b40508a
Aditya Panda <adityapandaofficial@gmail.com>
2021-05-12 19:16:53 +0530

    added:  mock server to app.js

--- app.js
+++ app.js
@@ -7,12 +7,22 @@
 var buttonTranslate = document.querySelector("#btn-translate")
 var textInput = document.querySelector("#txt-input")
 var outputDiv = document.querySelector("#output-text")
-
+var servUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 // console.log(buttonTranslate); 
+
+function getTranslateUrl(text){
+      return servUrl + "?text=" + text
+}
 
 buttonTranslate.addEventListener("click", function clickEventHandler(){
     // console.log("clicked")
     // console.log("input", textInput.value);
 
-    outputDiv.innerText = textInput.value; 
+    // outputDiv.innerText = textInput.value; 
+
+    var inputText = textInput.value
+    fetch(getTranslateUrl(inputText))
+         .then(response => response.json())
+         .then(json => console.log(json.contents.translated))
+
 })
